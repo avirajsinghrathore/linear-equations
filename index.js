24539648545c960b207b10fe2fe948ee7e156d59
ASKEW0 <avirajsinghrathore44@gmail.com>
2022-11-13 06:45:22 +0530

    ..
    ..

---
+++ index.js
@@ -0,0 +1,11 @@
+function program() {
+    let a = document.getElementById("a")[0].value;
+    // let b = document.getElementById("b")[0].value;
+    // let c = document.getElementById("c")[0].value;
+
+    // let p = document.getElementById("p")[0].value;
+    // let q = document.getElementById("q")[0].value;
+    // let r = document.getElementById("r")[0].value;
+    let sum = Number(a);
+    document.getElementById("answer").value = sum;
+}
