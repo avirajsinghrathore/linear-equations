6ad2248e12e4393a84884e1e67e06fe969e9ad07
ASKEW0 <avirajsinghrathore44@gmail.com>
2022-11-14 07:40:47 +0530

    .
    .

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ function program() {
    let a = document.getElementById("a").value;
    let b = document.getElementById("b").value;
    let c = document.getElementById("c").value;
-   let abc = a+"x+"+b+"y+"+c;
+   let abc = a+" x + "+b+" y + "+c;
     document.getElementById("abc").innerText = abc;
 
    let p = document.getElementById("p").value;
    let q = document.getElementById("q").value;
    let r = document.getElementById("r").value;
-   let pqr = p+"x+"+q+"y+"+r;
+   let pqr = p+" x +"+q+" y + "+r;
     document.getElementById("pqr").innerText = pqr;
 
     let x = a / p;
